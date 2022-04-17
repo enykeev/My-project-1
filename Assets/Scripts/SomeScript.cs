b38853ecab460598440d9cf9816b13d705b54adc
Kirill Enykeev <kirill@scalyr.com>
2022-04-17 18:27:06 +0200

    keep rays drawn

--- Assets/Scripts/SomeScript.cs
+++ Assets/Scripts/SomeScript.cs
@@ -14,10 +14,16 @@ public class SomeScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 parentPos = this.parentObject.transform.position;
+        Vector3 parentPos = this.parentObject.transform.position + Vector3.down;
         Vector3 targetPos = this.targetObject.transform.position;
-        bool obstructed = Physics.Raycast(parentPos, targetPos, Mathf.Infinity);
-        Debug.Log(this.parentObject.name + " -> " + this.targetObject.name + " is " + Vector3.Distance(parentPos, targetPos) + " and LOS is " + obstructed);
-        Debug.DrawLine(parentPos, targetPos, obstructed ? Color.red : Color.green);
+        Ray ray = new Ray(parentPos, targetPos - parentPos);
+        float distance = Vector3.Distance(parentPos, targetPos);
+        RaycastHit hitInfo;
+        bool obstructed = Physics.Raycast(ray, out hitInfo, distance, ~(1 << 10));
+        Debug.DrawRay(ray.origin, ray.direction * distance, obstructed ? Color.red : Color.green, 10);
+
+        if (obstructed) {
+            Debug.Log(hitInfo.collider.name);
+        }
     }
 }
